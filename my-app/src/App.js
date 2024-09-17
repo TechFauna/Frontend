cdd5a92b65e7d4bff79b6c997dacb167f4a0ce03
Matheus Frota <matheusfrotacsc@edu.unifor.br>
2024-09-17 20:17:58 -0300

    Criação de pasta do Backend e arquivos server.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,11 +11,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/recintos" element={<Recintos />} />
-
-
-
-
-
         
       </Routes>
     </Router>
