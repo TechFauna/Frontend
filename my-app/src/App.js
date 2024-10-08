04c384db00182f6c72bd1737dd90ab6161d560c8
Matheus Frota <matheusfrotacsc@edu.unifor.br>
2024-10-08 01:59:45 -0300

    Validação de login por NodeJS e Express e Autenticação do BD Supabase

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Recintos from './pages/Recintos';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/recintos" element={<Recintos />} />
-        
-      </Routes>
+      <header className="navbar">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Cadastro</Link>
+          <Link to="/recintos">Recintos</Link>
+        </nav>
+      </header>
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/recintos" element={<Recintos />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
